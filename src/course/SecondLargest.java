02e9fb7467b7b8bbfd7e8db897553b654f09cbf4
Sannu Sharma <99250959+Sannusharma02@users.noreply.github.com>
2026-05-25 11:13:35 +0000

    secondlargestele

--- src/course/SecondLargest.java
+++ src/course/SecondLargest.java
@@ -1,6 +1,6 @@
 public class SecondLargest{
     public static void main(String[] args){
-        int[] arr = {1, 2, 3, 4, 5, 9,8};
+        int[] arr = {1, 2, 3, 4, 5, 9,8,17,30,41,19,16,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
         int secondLargest = findSecondLargest(arr);
         System.out.println("The second largest element is: " + secondLargest);
     }
