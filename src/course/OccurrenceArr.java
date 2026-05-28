cdd68dd29659856a5883a8665a0f58125bd95705
Sannu Sharma <99250959+Sannusharma02@users.noreply.github.com>
2026-05-28 22:31:39 +0530

    Dsa with few blind 75 questions solved

--- src/course/OccurrenceArr.java
+++ src/course/OccurrenceArr.java
@@ -6,17 +6,18 @@ public class OccurrenceArr {
 
     public static void main(String[] args) {
         int[] nums = {1,2,3,1,2,3,2,6,7};
-        occurrenceArr(nums);
+       // occurrenceArr(nums);
     }
 
-    public static void occurrenceArr(int[] arr) {
-for (int i)
-        for (int j : arr) {
-            int count = 0;
-            if (current == j) {
-                count++;
-            }
-            System.out.println(j + " " + count);
-        }
-    }
+//    public static void occurrenceArr(int[] arr) {
+////for (int i=0;i<arr.length;i++){
+//        for (int j : arr) {
+//            int count = 0;
+//            if (current == j) {
+//                count++;
+//            }
+//            System.out.println(j + " " + count);
+//        }
+//    }?
+//}}
 }
