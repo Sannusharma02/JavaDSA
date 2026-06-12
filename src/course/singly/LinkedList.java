5f28a8ee6ab691ad184098ecb9246745ccb02b66
Sannu Sharma <99250959+Sannusharma02@users.noreply.github.com>
2026-06-12 09:47:07 +0000

    work

--- src/course/singly/LinkedList.java
+++ src/course/singly/LinkedList.java
@@ -1,4 +1,5 @@
 package course.singly;
+import java.util.Scanner;
 
 public class LinkedList {
 
@@ -113,26 +114,90 @@ public class LinkedList {
         if(position<0){
             System.out.println("Invalid position");
         }
+         
         if(position==0){
             head=head.next;
+            return;
         }
-        for(int i=0;i< position-1 && current!= null; i++){
+         
+        for(int i=0; i < position-1 && current != null; i++){
             current = current.next;
         }
 
+         if(current==null || current.next==null){
+             System.out.println("Invalid position! Out of bounds");
+             return;
+         }
 
+         current.next = current.next.next;  
      }
 
-    public static void main(String [] args){
-        LinkedList list = new LinkedList();
-        list.insertAtBeginning(10);
-        list.insertAtBeginning(20);
-        list.insertAtBeginning(30);
-        list.insertAtEnd(40);
-        list.insertAtPosition(50,2);
-        list.insertAtPosition(51,1);
-        list.insertAtPosition(52,0);
-        list.printData();
+    public boolean searchData(int data){
+        Node current = head;
+        while(current!=null){
+            if(current.data==data)
+                return true;
+        }
+        return false;
     }
+
+    public static void printMenu(){
+        System.out.println("********** Menu **********");
+        System.out.println("Enter 1 To Insert");
+        System.out.println("Enter 2 To Delete");
+        System.out.println("Enter 3 To Search");
+        System.out.println("Enter 4 To EXIT");
+        System.out.println("**************************");
+    }
+
+    public static void printInsertMenu(){
+        System.out.println("Press 1 to insert at head");
+        System.out.println("Press 2 to insert at tail");
+        System.out.println("Press 3 to insert at specific position");
+    }
+
+    public static void printDeleteMenu(){
+         System.out.println("Press 1 to delete from head");
+         System.out.println("Press 2 to delete from tail");
+         System.out.println("Press 3 to delete from specific position");
+    }
+    
+    public static void main(String [] args){
+        System.out.println("********** Welcome to Linked List App **********");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            try{
+                printMenu();
+                int choice = scanner.nextInt();
+                if (choice == 1){
+                    
+                } else if(choice == 2){
+                    
+                } else if(choice == 3){
+                    
+                } else if(choice == 4){
+                    
+                } else {
+                    System.out.println("Invalid choice! Please enter a valid option.");
+                }
+            } catch(Exception e){
+                System.out.println("Invalid input! Please enter a valid number.");
+            } finally{
+                
+            }
+        }
+    }
+    
+    // public static void main(String [] args){
+    //     LinkedList list = new LinkedList();
+    //     list.insertAtBeginning(10);
+    //     list.insertAtBeginning(20);
+    //     list.insertAtBeginning(30);
+    //     list.insertAtEnd(40);
+    //     list.insertAtPosition(50,2);
+    //     list.insertAtPosition(51,1);
+    //     list.insertAtPosition(52,0);
+    //     list.printData();
+    // }
     
 }
