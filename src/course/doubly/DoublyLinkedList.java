f780f574de7e14a40078f2963c2116b267c3a082
Sannu Sharma <99250959+Sannusharma02@users.noreply.github.com>
2026-06-17 11:16:11 +0000

    work

---
+++ src/course/doubly/DoublyLinkedList.java
@@ -0,0 +1,64 @@
+package course.doubly;
+
+public class DoublyLinkedList{
+  private Node head;
+  private Node tail;
+
+  public DoublyLinkedList(){
+    head = null;
+    tail = null;
+  }
+
+  public void addFront(int data){
+    Node newNode = new Node(data);
+    if(head==null){
+      head = newNode;
+      tail = newNode;
+    } else {
+      head.prev = newNode;
+      newNode.next =head;
+      head = newNode;
+    }
+  }
+
+  public void addEnd(int data){
+    Node newNode = new Node(data);
+    if(tail==null){
+      head=tail=newNode;
+    } else{
+      tail.next=newnode;
+      newNode.prev=tail;
+      tail=newNode; 
+    }
+  }
+
+  public void addAtPosition(int data, int position){
+    if(position<0) {
+      System.out.println("Invalid position");
+      return;
+    }
+    if(position==0){
+      addFront(data);
+      return;
+    }
+    Node current = head;
+    int index = 0;
+    while(current!=null && index<position-1){
+      current = current.next;
+      index++;
+    }
+    if(current==null){
+      System.out.println("Invalid position! Out of bounds");
+      return;
+    }
+    Node newNode = new Node(data);
+    newNode.next = current.next;
+    newNode.prev = current;
+    if(current.next!=null){
+      current.next.prev = newNode;
+    } else
+      tail = newNode;
+    current.next = newNode;
+  }
+  
+}
---
+++ src/course/doubly/Node.java
@@ -0,0 +1,15 @@
+package course.doubly;
+
+public class Node {
+  
+  Node prev = null;
+  int data;
+  Node next = null;
+  
+  public Node(int data){
+    this.prev = null;
+    this.data = data;
+    this.next = null;
+  }
+  
+}
